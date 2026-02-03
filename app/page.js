0239f8286c0a977026388bd0ed0bb18a7d532ee9
Juan Ignacio <juan.ignacio.gutierrez@fing.edu.uy>
2026-02-03 10:49:05 -0300

    Terminar estilos del Home

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import bannerImg from "../public/images/banner-img.png";
+import titleImg from "../public/images/ironfuel-title.png";
+import img1 from "../public/images/why-choose-us1.png";
+import img2 from "../public/images/why-choose-us2.jpg";
+import img3 from "../public/images/why-choose-us3.jpg";
 
 export default function Home() {
   return (
     <>
       <div className={styles.banner}>
-        {/* <Image src={bannerImg} alt="Shaker de proteina" /> */}
-        <h1 className="banner-title">IronFuel</h1>
-        <h3 className="banner-subtitle">
-          El combustible que tu cuerpo necesita para superar cada entrenamiento.
-        </h3>
-        <button>Ver nuestro catalogo</button>
+        <Image src={titleImg} alt="IronFuel" className={styles["banner-img"]} />
+
+        <button>Ver nuestro catálogo</button>
       </div>
       <div className={styles.search}>
         <h1>Encuentra tu suplemento ideal</h1>
@@ -35,7 +35,7 @@ export default function Home() {
                 cy="8"
                 r="6"
                 stroke="white"
-                stroke-width="2"
+                strokeWidth="2"
                 fill="none"
               />
               <line
@@ -44,61 +44,71 @@ export default function Home() {
                 x2="18"
                 y2="18"
                 stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
+                strokeWidth="2"
+                strokeLinecap="round"
               />
             </svg>
           </button>
         </form>
       </div>
       <div className={styles["why-choose-us"]}>
-        <h2>¿Por qué elegir IronFuel?</h2>
-        <ul>
+        <h1>¿Por qué elegir IronFuel?</h1>
+        <ul className={styles["why-choose-us__items"]}>
           <li>
-            <h3>Calidad garantizada</h3>
-            <p>
-              Nuestros suplementos cumplen con los más altos estándares de
-              calidad de la industria. Cada producto que ofrecemos pasa por
-              rigurosos controles de calidad que verifican pureza, potencia y
-              composición. No comprometemos la efectividad de nuestros
-              suplementos: lo que ves en la etiqueta es exactamente lo que
-              obtienes en cada porción. Trabajamos únicamente con productos que
-              cuentan con certificaciones internacionales y que han sido
-              fabricados en instalaciones que siguen las buenas prácticas de
-              manufactura (GMP), garantizando que cada frasco, sobre o barra que
-              llega a tus manos cumple con estándares farmacéuticos.
-            </p>
+            <h3>Calidad Garantizada</h3>
+            <div className={styles["why-choose-us__item"]}>
+              <p>
+                Nuestros suplementos cumplen con los más altos estándares de
+                calidad de la industria. Cada producto que ofrecemos pasa por
+                rigurosos controles de calidad que verifican pureza, potencia y
+                composición. No comprometemos la efectividad de nuestros
+                suplementos: lo que ves en la etiqueta es exactamente lo que
+                obtienes en cada porción. Trabajamos únicamente con productos
+                que cuentan con certificaciones internacionales y que han sido
+                fabricados en instalaciones que siguen las buenas prácticas de
+                manufactura (GMP), garantizando que cada frasco, sobre o barra
+                que llega a tus manos cumple con estándares farmacéuticos.
+              </p>
+              <Image src={img1} alt="" />
+            </div>
           </li>
-          <li>
+          <li className={styles["why-choose-us__highlighted"]}>
             <h3>Marcas de Renombre Mundial</h3>
-            <p>
-              Colaboramos exclusivamente con las marcas más reconocidas y
-              respetadas de la industria de suplementación deportiva a nivel
-              global. Marcas líderes que han construido su reputación durante
-              décadas gracias a la innovación, investigación científica y
-              resultados comprobados. Estas marcas no solo son populares, sino
-              que son la elección de atletas profesionales, culturistas de élite
-              y entusiastas del fitness en todo el mundo. Al elegir nuestros
-              productos, accedes al mismo nivel de calidad que utilizan los
-              mejores deportistas del planeta.
-            </p>
+            <div className={styles["why-choose-us__item"]}>
+              <Image src={img2} alt="" />
+              <p>
+                Colaboramos exclusivamente con las marcas más reconocidas y
+                respetadas de la industria de suplementación deportiva a nivel
+                global. Marcas líderes que han construido su reputación durante
+                décadas gracias a la innovación, investigación científica y
+                resultados comprobados. Estas marcas no solo son populares, sino
+                que son la elección de atletas profesionales, culturistas de
+                élite y entusiastas del fitness en todo el mundo. Al elegir
+                nuestros productos, accedes al mismo nivel de calidad que
+                utilizan los mejores deportistas del planeta.
+              </p>
+            </div>
           </li>
           <li>
             <h3>Miles de Clientes Satisfechos</h3>
-            <p>
-              Nuestra comunidad de clientes satisfechos es nuestro mejor aval.
-              Cientos de personas han transformado su físico, mejorado su
-              rendimiento y alcanzado sus metas fitness gracias a nuestros
-              productos y asesoramiento. Las reseñas positivas, testimonios y
-              casos de éxito hablan por sí solos: desde principiantes que dieron
-              sus primeros pasos en el gimnasio hasta atletas experimentados que
-              buscaban ese empujón extra para superar sus límites. Cada día
-              recibimos comentarios de clientes que han notado mejoras en su
-              recuperación muscular, aumento de fuerza, pérdida de grasa o
-              ganancia de masa magra. Tu satisfacción es nuestra prioridad, y
-              trabajamos constantemente para mantener la confianza que has
-              depositado en nosotros.
-            </p>
+            <div className={styles["why-choose-us__item"]}>
+              <p>
+                Nuestra comunidad de clientes satisfechos es nuestro mejor aval.
+                Cientos de personas han transformado su físico, mejorado su
+                rendimiento y alcanzado sus metas fitness gracias a nuestros
+                productos y asesoramiento. Las reseñas positivas, testimonios y
+                casos de éxito hablan por sí solos: desde principiantes que
+                dieron sus primeros pasos en el gimnasio hasta atletas
+                experimentados que buscaban ese empujón extra para superar sus
+                límites. Cada día recibimos comentarios de clientes que han
+                notado mejoras en su recuperación muscular, aumento de fuerza,
+                pérdida de grasa o ganancia de masa magra. Tu satisfacción es
+                nuestra prioridad, y trabajamos constantemente para mantener la
+                confianza que has depositado en nosotros.
+              </p>
+
+              <Image src={img3} alt="" />
+            </div>
           </li>
         </ul>
       </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,84 @@
+"use client";
 import Image from "next/image";
 import classes from "./Header.module.css";
+import headerImg from "../public/images/ironfuel-header.png";
+import logo from "../public/images/ironfuel-header.png";
+import { useState } from "react";
 
 export default function Header() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+  function handleToggle() {
+    setNavIsOpen((prev) => !prev);
+  }
   return (
-    <header className={classes.header}>
-      <h1>IronFuel</h1>
-      <ul className={classes["nav-items"]}>
-        <li>Suplementos</li>
-        <li>Vitaminas</li>
-        <li>Accesorios</li>
-        <li>
-          <button className={classes["nav-items__button"]}>Mi carro (0)</button>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className={classes.header}>
+        <Image
+          src={headerImg}
+          alt="IronFuel"
+          className={classes["header-img"]}
+        />
+        <div className={classes["toggle"]}>
+          <button className={classes["toggle-button"]} onClick={handleToggle}>
+            <span className={classes["toggle-button__bar"]}></span>
+            <span className={classes["toggle-button__bar"]}></span>
+            <span className={classes["toggle-button__bar"]}></span>
+          </button>
+        </div>
+
+        <ul className={classes["nav-items"]}>
+          <li>Suplementos</li>
+          <li>Vitaminas</li>
+          <li>Accesorios</li>
+          <li>
+            <button className={classes["nav-items__button"]}>
+              Mi carro (0)
+            </button>
+          </li>
+        </ul>
+      </header>
+      {navIsOpen && (
+        <>
+          <div className={classes.backdrop}></div>
+          <nav className={classes["mobile-nav"]}>
+            <button className={classes["close-button"]} onClick={handleToggle}>
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M25 7L7 25M7 7L25 25"
+                  stroke="white"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </button>
+            <ul className={classes["mobile-nav__items"]}>
+              <li className={classes["mobile-nav__item"]}>
+                <a href="packages/index.html">Suplementos</a>
+              </li>
+              <li className={classes["mobile-nav__item"]}>
+                <a href="customers/index.html">Vitaminas</a>
+              </li>
+              <li className={classes["mobile-nav__item"]}>
+                <a href="customers/index.html">Accesorios</a>
+              </li>
+              <li className={classes["mobile-nav__item--cta"]}>
+                <a href="start-hosting/index.html">Mi carro (0)</a>
+              </li>
+            </ul>
+            <Image
+              src={logo}
+              alt="ironFuel logo"
+              className={classes["mobile-nav__img"]}
+            />
+          </nav>
+        </>
+      )}
+    </>
   );
 }
